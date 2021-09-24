6577bf92e42d1ca2875a99a80b57b6d9abc56016
sangharsh23 <sangharsh355@gmail.com>
2021-09-25 00:30:29 +0530

    Added : welcome msg

---
+++ src/com/bridgelabz/employee/wage/EmployeeWageBuilder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employee.wage;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the employee wage computation program");
+	}
+
+}
